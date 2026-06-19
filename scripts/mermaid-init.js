58b92bad2f78234e8eb8ff99e244ed3f277ce98d
Natur3-Boy <55642761+Natur3-Boy@users.noreply.github.com>
2026-06-19 15:22:14 -0400

    Create scripts/mermaid-init.js

---
+++ scripts/mermaid-init.js
@@ -0,0 +1,7 @@
+import mermaid from 'https://cdn.jsdelivr.net/npm/mermaid@11.4.1/dist/mermaid.esm.min.mjs';
+
+mermaid.initialize({ 
+  startOnLoad: true,
+  securityLevel: 'loose',
+  theme: 'default' 
+});   
